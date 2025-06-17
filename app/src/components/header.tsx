7abbac973f2cd137e3e1f6e902f9bceadbf91a42
thibaut denis <thibaut_du_denis@hotmail.fr>
2025-06-17 14:14:16 +0200

    premiere creation de composant /commit debut de configuration CI CD

---
+++ app/src/components/header.tsx
@@ -0,0 +1,16 @@
+
+export default function Header (){
+
+    return (
+        <>
+            <div className="flex flex-row justify-around">
+                <p>LT Coaching</p>
+                <ul className="flex flex-row">
+                    <li>Home</li>
+                    <li>Services</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        </>
+    )
+}
---
+++ app/src/components/separation.tsx
@@ -0,0 +1,8 @@
+export default function Separation (){
+
+    return(
+        <div className="m-8">
+            {/* trait dore */}
+        </div>
+    )
+}
