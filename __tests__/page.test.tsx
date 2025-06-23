4db62013f331f7ba276db349232f9dfcf1cdf1d6
ThibautTobi <95364649+ThibautTobi@users.noreply.github.com>
2025-06-23 21:44:58 +0200

    Dev (#2) configuration ci / cd . reste husky a intégrer
    * creation du workflows GitHub / creation Branch "Dev"
    
    * configuration du ci . avant configuration Husky
    
    * test husky v 9
    
    * test erreur TS
    
    * test erreur TS
    
    * test erreur TS
    
    * test erreur dans page
    
    * test ESLint et Prettier
    
    * desinstalation de Husky temporairement
    
    * configuration ci et test validé / reste husky v 9 + lint-staged a intégrer
    
    * test ajout element shadcn test
    
    * changement couleur theme shadcn
    
    ---------
    
    Co-authored-by: thibaut denis <thibaut_du_denis@hotmail.fr>

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -1,12 +1,16 @@
 import { render, screen } from '@testing-library/react';
-import Home from '../app/page';
+import Home from '@/app/page';
 import '@testing-library/jest-dom';
 
+// Test du rendu de base
 describe('Home Page', () => {
   it('affiche le titre et le composant Header', () => {
     render(<Home />);
 
-    // Vérifie la présence du titre
-    expect(screen.getByText('test')).toBeInTheDocument();
+    // Vérifie que le titre est affiché
+    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent('test');
+
+    // Vérifie que le paragraphe est affiché
+    expect(screen.getByText(/Lorem ipsum/)).toBeInTheDocument();
   });
 });
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="max-w-2xl ">
       <h1>test</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur </p>
     </div>
   );
 }
