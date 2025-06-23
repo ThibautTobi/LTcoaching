72416b94f26664869cbcf728ab7478976a9e5a0b
thibaut denis <thibaut_du_denis@hotmail.fr>
2025-06-23 10:55:35 +0200

    test erreur dans page

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div className="max-w-2xl ">
       <h1>test</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore odio est dolorem, inventore quis quasi qui suscipit explicabo velit quo at optio commodi expedita recusandae id tenetur numquam assumenda deserunt fugit harum eveniet veritatis vel autem natus. Repellat inventore nemo sunt. Nulla nobis magni, corporis similique error minima sint quae deleniti suscipit reprehenderit velit quia quam eum perferendis placeat maxime est magnam maiores nam in iure? Porro provident alias dignissimos, sint est culpa suscipit. Amet cum exercitationem, adipisci autem quo harum quas.</p>
     </div>
   );
 }
