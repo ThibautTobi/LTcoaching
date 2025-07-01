08aa2d685e2d894dea111ea2d1e5b32b7df754cc
thibaut denis <thibaut_du_denis@hotmail.fr>
2025-07-01 14:37:48 +0200

    création contenue page Hero : test husky lint

---
+++ app/src/components/hero.tsx
@@ -0,0 +1,20 @@
+export default function Hero() {
+  return (
+    <>
+      <h2>test h2</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam nam
+        inventore aut molestiae, optio, error qui quibusdam blanditiis delectus
+        voluptatibus quos repellendus? Sint ab maxime dolores magnam minima
+        nesciunt, iste repellat sequi, voluptate molestias cupiditate numquam
+        laudantium modi officia quidem sunt eum dignissimos. Repellendus
+        sapiente esse nesciunt eaque expedita placeat quasi eos quas consectetur
+        odio. Ea delectus tempore illo enim eligendi velit vitae sequi aut ab
+        cupiditate aliquid molestias, ut non nostrum illum quis qui laborum
+        odit. Perferendis consequatur ullam accusamus placeat, omnis harum ea
+        sit porro nisi tempore unde vel amet architecto similique enim expedita
+        ut reprehenderit!
+      </p>
+    </>
+  );
+}
