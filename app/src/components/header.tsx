013c9682a15b3f614dff57543fa3c9cb1cd71c83
thibaut denis <thibaut_du_denis@hotmail.fr>
2025-07-18 14:47:54 +0200

    modification du composant Header de la page.

--- app/src/components/header.tsx
+++ app/src/components/header.tsx
@@ -1,72 +1,30 @@
-'use client';
+/**
+ * @file Header.tsx
+ * Composant Header avec Logo et import du composant de navigation principal de l'application.
+ * Gère l'affichage du menu responsive (desktop & mobile).
+ */
 
-import { useState } from 'react';
-import Link from 'next/link';
-import { Button } from '@/src/components/ui/button';
-import { Menu, X } from 'lucide-react';
+import NavBar from './navBar';
 
-const links = [
-  { name: 'Accueil', href: '/' },
-  { name: 'À propos', href: '/a-propos' },
-  { name: 'Services', href: '/services' },
-  { name: 'Tarifs', href: '/tarifs' },
-  { name: 'Contact', href: '/contact' },
-];
+/**
+ * Composant Header : Affiche une barre de navigation responsive
+ * @returns {JSX.Element} Le header avec navigation desktop et mobile
+ */
+
 export default function Header() {
-  const [isOpen, setIsOpen] = useState(false);
-
   return (
-    <header className="w-full">
-      <div>{/*logo*/}</div>
-      <nav>
-        {/* Menu burger visible uniquement sur mobile */}
-        <Button
-          className="md:hidden"
-          onClick={() => setIsOpen(!isOpen)}
-          aria-label="Menu mobile"
-        >
-          {isOpen ? <X size={24} /> : <Menu size={24} />}
-        </Button>
-
-        {/* Menu desktop */}
-        <ul className="hidden mb:flex">
-          {links.map((link) => (
-            <li key={link.name}>
-              <Link href={link.href} className="hover:text-white">
-                {link.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-
-      {/* Menu mobile déroulant */}
-
-      {isOpen && (
-        <div className="mb:hidden">
-          <ul>
-            {links.map((link) => (
-              <li key={link.name}>
-                <Link
-                  href={link.href}
-                  className="hover:text-white"
-                  onClick={() => setIsOpen(false)}
-                >
-                  {link.name}
-                </Link>
-              </li>
-            ))}
-          </ul>
+    <header className="w-full bg-background sticky top-0 z-50 border-b-2 border-primary mb-10">
+      <div className="max-w-6xl mx-auto px-4 flex items-center justify-between h-16">
+        {/* Logo */}
+        <div className="text-xl font-bold">
+          <p>MonLogo</p>
+          {/* <Image 
+            href={}
+            aria-label='Logo LTcoaching'
+          /> */}
         </div>
-      )}
+        <NavBar />
+      </div>
     </header>
-    // <div className="flex flex-row justify-around max-w-max h-40 bg-slate-600">
-    //   <p>LT Coaching</p>
-    //   <ul className="flex flex-row">
-    //     <li>Home</li>
-    //     <li>Services</li>
-    //     <li>Contact</li>
-    //   </ul>
-    // </div>
   );
 }
