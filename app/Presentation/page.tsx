650d40c0ee3637f48780ceaea5735f22735994f8
thibaut denis <thibaut_du_denis@hotmail.fr>
2025-07-18 14:38:15 +0200

    creation de la page presentation.

---
+++ app/Presentation/page.tsx
@@ -0,0 +1,21 @@
+import { Button } from '@/src/components/ui/button';
+
+export default function Présentation() {
+  return (
+    <div>
+      <section>
+        <h2>titre 2</h2>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa fugiat
+          quis dicta quasi assumenda pariatur blanditiis est cum facere! Velit
+          exercitationem magni similique, atque ab animi eveniet rem distinctio
+          doloribus possimus cupiditate consequuntur laboriosam soluta et eos
+          laborum, aut, facilis ratione esse. Voluptate eum unde totam officia
+          nulla sed iusto modi culpa nam error qui aliquam, laudantium esse
+          aspernatur fuga in corrupti enim. Vel!
+        </p>
+        <Button> Contacté nous </Button>
+      </section>
+    </div>
+  );
+}
