90142d1f1daef1d687076ca57bfeb2ebe2c99e45
thibaut denis <thibaut_du_denis@hotmail.fr>
2025-07-22 21:23:26 +0200

    integration du logo

--- app/src/components/header.tsx
+++ app/src/components/header.tsx
@@ -5,7 +5,8 @@
  */
 
 import NavBar from './navBar';
-
+import Image from 'next/image';
+import Logo from '@/public/LTcoaching-sansBG.png';
 /**
  * Composant Header : Affiche une barre de navigation responsive
  * @returns {JSX.Element} Le header avec navigation desktop et mobile
@@ -17,11 +18,7 @@ export default function Header() {
       <div className="max-w-6xl mx-auto px-4 flex items-center justify-between h-16">
         {/* Logo */}
         <div className="text-xl font-bold">
-          <p>MonLogo</p>
-          {/* <Image 
-            href={}
-            aria-label='Logo LTcoaching'
-          /> */}
+          <Image src={Logo} width={80} height={80} alt="Logo LTcoaching" />
         </div>
         <NavBar />
       </div>
