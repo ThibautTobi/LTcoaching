a840e225cd1f684eba89e34ec17405bd8adf1147
thibaut denis <thibaut_du_denis@hotmail.fr>
2025-07-22 21:06:37 +0200

    mise a jour pour corrgier des compatibiliter de test sur les composant de lucide qui intervienne avec shadcn

--- jest.config.ts
+++ jest.config.ts
@@ -7,20 +7,16 @@ const createJestConfig = nextJest({
 
 const config: Config = {
   coverageProvider: 'v8',
-  preset: 'ts-jest',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   transformIgnorePatterns: ['/node_modules/(?!(lucide-react)/)'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
-    '^lucide-react$': '<rootDir>/__mocks__/lucide-react.ts',
-    '\\.(css|scss|sass)$': 'identity-obj-proxy',
   },
 
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
 
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
